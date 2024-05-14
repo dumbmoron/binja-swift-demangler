ba16c03a42b9d4ab0fc5c19fea2888edf0723114
jj <log@riseup.net>
2024-05-14 15:04:30 +0200

    refactor

--- __init__.py
+++ __init__.py
@@ -1,36 +1,33 @@
 import binaryninja
 import subprocess
 
-def run_demanger(symbols):
+def run_demangler(symbols):
 	return subprocess.check_output(["swift", "demangle", "--compact", "--simplified"], input="\n".join(symbols).encode()).decode().strip().split("\n")
 
 def demangle_swift(bv):
 	swift_functions = []
-	for function in bv.functions:
-		if function.name.startswith("_$s"):
-			swift_functions.append(function)
-	results = run_demanger(map(lambda f: f.name, swift_functions))
-	assert len(swift_functions) == len(results)
+	results = run_demangler(map(lambda f: f.name, bv.functions))
+	demangled_func_count = 0
 	for (function, name) in zip(swift_functions, results):
+		if name == function:
+			continue
+
+		demangled_func_count += 1
 		if function.comment:
 			function.comment = f"{function.comment} ({function.name})"
 		else:
 			function.comment = function.name
 		function.name = name
 
-	variables = bv.data_vars
-	swift_variables = []
-	for variable in variables:
-		try:
-			variable = variables[variable]
-			if variable.name.startswith("_$s"):
-				swift_variables.append(variable)
-		except:
-			pass
-	results = run_demanger(map(lambda v: v.name, swift_variables))
-	assert len(swift_variables) == len(results)
-	for (variable, name) in zip(swift_variables, results):
-		variable.name = name
-	binaryninja.log_info(f"Swift demangling complete! Updated {len(swift_functions)} functions and {len(swift_variables)} variables.")
+	variables = bv.data_vars.values()
+	results = run_demangler(map(lambda v: v.name, variables))
+	demangled_var_count = 0
+
+	for (variable, demangled_name) in zip(swift_variables, results):
+		if variable.name != demangled_name:
+			variable.name = demangled_name
+			demangled_var_count += 1
+			
+	binaryninja.log_info(f"Swift demangling complete! Updated {len(demangled_func_count)} functions and {len(demangled_var_count)} variables.")
 
 binaryninja.PluginCommand.register("Swift Demangler", "Demangles Swift", demangle_swift)
